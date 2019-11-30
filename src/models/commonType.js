7a6ef8bc3645a36dcd912e1882b4f98327b5cc0a
L.SY <892462856@qq.com>
2019-11-30 14:38:09 +0800

    messageStorage.js

--- src/models/commonType.js
+++ src/models/commonType.js
@@ -4,11 +4,17 @@ export const MessageType = {
   image: 2,
   video: 3,
   file: 4,
+  typing: '5',
+  read: '6',
+  unknown: '7',
   0: 'sys',
   1: 'text',
   2: 'image',
   3: 'video',
-  4: 'file'
+  4: 'file',
+  5: 'typing',
+  6: 'read',
+  7: 'unknown'
 }
 
 export default {
--- src/storage/messageStorage.js
+++ src/storage/messageStorage.js
@@ -1,53 +1,46 @@
-import { conversationApi, MessagesApi } from '@/dataAccessor/RongApi'
-import { MessageConvertToConversation } from '@/assets/js/Types'
+import localforage from 'localforage'
+import messager from '@/datasources/message'
+import { MessageConvertToConversation } from '@/models'
 
-/**
- * 我的会话列表存储管理器
- * @param {*} userId 我的ID
- */
-function ConversationStorage (userId)
+localforage.config({
+  // driver: localforage.WEBSQL, // 使用 WebSQL；也可以使用 setDriver()
+  name: 'hiChat',
+  version: 1.0,
+  // size: 4980736, // 数据库的大小，单位为字节。现仅 WebSQL 可用
+  storeName: 'keyvaluepairs', // 仅接受字母，数字和下划线
+  description: 'hiChat web cache storage'
+})
+
+class ConvrStorage
 {
-  this._key = `conversationStorage_${userId}`
-  this._cache = window.localStorage
-  this._serverApi = conversationApi
-}
-ConversationStorage.prototype = {
+  constructor(userId)
+  {
+    this._key = `convr_${userId}`
+    this._cache = localforage // window.localStorage
+  }
   get ()
   {
-    const list = JSON.parse(this._cache.getItem(this._key))
-    if (!list)
-    {
-      return this._serverApi.getList(null, 100000).then(data =>
-      {
-        // if (data.length === 0) return Promise.resolve([])
-        data = [] // 暂时不要rong的会话，因为未开通rong的历史消息 功能，拿不到消息，只有会话没用
-        return this.save(data)
-      })
-    }
-    return Promise.resolve(list)
-  },
+    return this._cache.getItem(this._key)
+  }
   remove ()
   {
-    return this.get().then((storage) =>
+    return this.get().then(storage =>
     {
-      this._cache.removeItem(this._key)
-      return storage
+      return this._cache.removeItem(this._key).then(() => storage)
     })
-  },
+  }
   save (storage)
   {
-    this._cache.setItem(this._key, JSON.stringify(storage))
-    return Promise.resolve(storage)
-  },
+    return this._cache.setItem(this._key, storage)
+  }
   getItem (targetId)
   {
     return this.get().then(storage =>
     {
-      // const item = storage.find(t => t.conversationType === conversationType && t.targetId === targetId)
       const item = storage.find(t => t.targetId === targetId)
       return { storage, item }
     })
-  },
+  }
   addItem (item)
   {
     const { targetId } = item
@@ -55,70 +48,63 @@ ConversationStorage.prototype = {
     {
       if (oldItem)
       {
-        if (item.unreadMessageCount !== 0)
+        if (item.unreadCount !== 0)
         {
-          item.unreadMessageCount = oldItem.unreadMessageCount + 1
+          item.unreadCount = oldItem.unreadCount + 1
         }
         storage.splice(storage.indexOf(oldItem), 1)
       }
       storage.unshift(item)
+      storage.sort((a, b) => a.sendTime - b.sendTime)
       this.save(storage)
       return item
     })
-  },
+  }
   updateItem (targetId, updater)
   {
-    this.get().then(list =>
+    return this.getItem(targetId).then(({ storage, item }) =>
     {
-      const item = list.find(t => t.targetId === targetId)
       if (item)
       {
-        updater(item)
-        this.save(list)
+        updater(item) // 一般不改sendTime,bu yong sort
+        return this.save(storage).then(() => item)
       }
+      return item
     })
-  },
+  }
   removeItem (targetId)
-  { // conversationType, targetId
+  {
     return this.getItem(targetId).then(({ storage, item }) =>
     {
       if (!item) return item
       storage.splice(storage.indexOf(item), 1)
-      this.save(storage)
-      this._serverApi.removeOneBy(item.conversationType, targetId)
-      return item
+      return this.save(storage).then(() => item)
     })
   }
 }
 
-/**
- * 我的消息存储管理器
- * @param {*} userId 我的ID
- * @param {*} target1 打开的聊天窗口target：好友/群
- */
-function MessageStorage (userId)
+class MessageStorage 
 {
-  // debugger
-  if (!userId) return null
-  this._cache = window.localStorage
-  this._userId = userId
-  this._key = `messageStorage_${userId}`
-  this._cStorage = new ConversationStorage(userId) // 会话是根据消息来的
-}
-MessageStorage.prototype = {
-  _msger (target)
+  constructor(userId)
   {
-    return new MessagesApi(target.conversationType, target.targetId)
-  },
+    this._cache = localforage
+    this._userId = userId
+    this._key = `msg_${userId}`
+    this._cStorage = new ConvrStorage(userId) // 会话是根据消息来的
+    this._msger = new messager(userId, (msg) =>
+    {
+      console.log(msg)
+    })
+  }
   _getFullKey (targetId)
   {
-    return `${this._key}_${targetId}` // 一个target存储一个localStorage
-  },
+    return `${this._key}_${targetId}`
+  }
   get (targetId)
   {
     const storage = JSON.parse(this._cache.getItem(this._getFullKey(targetId))) || []
     return Promise.resolve(storage)
-  },
+  }
   remove (targetId)
   {
     return this.get(targetId).then((storage) =>
@@ -126,12 +112,12 @@ MessageStorage.prototype = {
       this._cache.removeItem(this._getFullKey(targetId))
       return storage
     })
-  },
+  }
   save (storage, targetId)
   {
     this._cache.setItem(this._getFullKey(targetId), JSON.stringify(storage))
     return Promise.resolve()
-  },
+  }
   /**
    * 保存+更新会话。return后{message消息,convr会话}
    * @param {*} msg
@@ -158,7 +144,7 @@ MessageStorage.prototype = {
 
       return { message: msg, convr, senderUser, target }
     })
-  },
+  }
   removeItem (msg)
   {
     return this.get(msg.targetId).then((storage) =>
@@ -167,12 +153,12 @@ MessageStorage.prototype = {
       this.save(storage, msg.targetId)
       return msg
     })
-  },
+  }
 
   _onSent (msg)
   {
     return this._saveItem(msg)
-  },
+  }
   sendText ({ content, extra, sender, target })
   {
     return this._msger(target).sendText({ content, extra }).then((message) =>
@@ -181,16 +167,7 @@ MessageStorage.prototype = {
       message.target = target
       return this._onSent(message)
     })
-  },
-  sendEncryptText ({ content, extra, sender, target })
-  {
-    return this._msger(target).sendEncryptText({ content, extra }).then((message) =>
-    {
-      message.senderUser = sender
-      message.target = target
-      return this._onSent(message)
-    })
-  },
+  }
   sendImage ({ content, imageUri, sender, target })
   {
     return this._msger(target).sendImage({ content, imageUri }).then((message) =>
@@ -199,7 +176,7 @@ MessageStorage.prototype = {
       message.target = target
       return this._onSent(message)
     })
-  },
+  }
   sendFile ({ name, fileUrl, extra, type, size, sender, target })
   {
     return this._msger(target).sendFile({ name, fileUrl, extra, type, size }).then((message) =>
@@ -208,73 +185,44 @@ MessageStorage.prototype = {
       message.target = target
       return this._onSent(message)
     })
-  },
+  }
   _onReceived (msg, read)
   {
     return this._saveItem(msg, read)
-  },
+  }
   receiveText (msg, read)
   {
     return this._onReceived(msg, read)
-  },
+  }
   receiveImage (msg, read)
   {
     return this._onReceived(msg, read)
-  },
+  }
   receiveFile (msg, read)
   {
     return this._onReceived(msg, read)
-  },
+  }
   receiveHQVoice (msg, read)
   {
     return this._onReceived(msg, read)
-  },
+  }
   receiveRichContent (msg, read)
   {
     return this._onReceived(msg, read)
-  },
+  }
   receiveGroupNotification (msg, read)
   {
     return this._onReceived(msg, read)
-  },
-  receiveTypingStatus ()
-  {
-    return Promise.resolve({ message: null, convr: null, senderUser: null, target: null })
-  },
-  receiveReadReceipt ()
-  {
-    return Promise.resolve({ message: null, convr: null, senderUser: null, target: null })
-  },
-  receiveUnknown ()
-  {
-    return Promise.resolve({ message: null, convr: null, senderUser: null, target: null })
-  },
-  receiveOther ()
-  {
-    return Promise.resolve({ message: null, convr: null, senderUser: null, target: null })
-  },
+  }
 
-  /**
-   * 获取 会话列表
-   */
   getConversations ()
   {
     return this._cStorage.get()
-  },
-  /**
-   * 修改一个会话
-   * @param {String} targetId
-   * @param {Function} updater 修改函数
-   */
+  }
   updateConversation (targetId, updater)
   {
     this._cStorage.updateItem(targetId, updater)
-  },
-  /**
-   * 移除一个会话
-   * @param {*} conversationType
-   * @param {*} targetId
-   */
+  }
   removeConversation (targetId)
   { // conversationType, targetId
     this.remove(targetId) // 先删除消息
